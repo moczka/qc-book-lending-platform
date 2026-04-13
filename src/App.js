9b72b00c5929e155a4123965c869ca5d104ddbff
moczka <steven.guesp@gmail.com>
2026-04-13 13:14:00 -0400

    Add react

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from '../media/quincy-college-library.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <header className="NavigationHeader">
+        <div className="NavigationHeader-content">
+            <a href="index.html" className="NavigationHeader-logoLink">
+                <div className="BrandLogo">
+                    <img src={logo} alt="Quincy College Logo" className="BrandLogo-image" />
+                    <h1 className="BrandLogo-title"><span className="BrandLogo-titleText">QUINCY COLLEGE</span><span className="BrandLogo-subtitle">BOOK LENDING</span></h1>
+                </div>
+            </a>
+            <ul className="NavigationHeader-pages">
+                <li><a href="index.html" className="NavigationHeader-link is-active">Search</a></li>
+                <li><a href="manage.html" className="NavigationHeader-link">Manage</a></li>
+            </ul>
+        </div>
+    </header>
+  );
+}
+
+export default App;
