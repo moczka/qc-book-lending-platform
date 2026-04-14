c68c735f7001f839a0be1b54d33be80e84003adc
moczka <steven.guesp@gmail.com>
2026-04-14 01:19:46 -0400

    Add functionality to search phase component

--- src/SearchPhase.jsx
+++ src/SearchPhase.jsx
@@ -5,20 +5,30 @@
   Search phase where students search and select books.
 
 */
+import { useState } from 'react';
 import Button from './Button';
 
 function SearchPhase() {
-    const handleClick = (event) => {
-        event.preventDefault();
-    };
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+    
+
     return (
         <div className="ArticleSection">
             <div class="ApplicationPhase" id="ApplicationSearchPhase">
                 <h3 class="ArticleSection-title">Search Course Materials</h3>
                 <p class="ArticleSection-paragraph">Find course materials such as books, access codes and other materials using the search bar below. You can search by book name, author, ISBN-13 or 10, course name, course id and course name. Select from the search results the books you would like to reserve.</p>
                 <form class="SearchTool">
-                    <input class="SearchTool-inputField" id="SearchTool-inputField" required={true} type="text" placeholder="Search for books"/>
-                    <Button onClick={handleClick}>
+                    <input class="SearchTool-inputField" value={searchQuery} onChange={(event) => {
+                        setSearchQuery(event.currentTarget.value);
+                    }} required={true} type="text" placeholder="Search for books"/>
+                    <Button onClick={(event) => { 
+                        event.preventDefault();
+                        // Retrieve books
+                          const searchResults = database.searchForBooks(searchQuery);
+                          setSearchResults(searchResults);
+                    }}>
                         <span class="material-symbols-outlined">search</span> Search
                     </Button>
                 </form>
